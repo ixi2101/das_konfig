4297d294b1eb6cf08e4f0761a89c496505f1db0a
Your Name <you@example.com>
2022-12-04 23:30:00 +0000

    update emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,6 @@
 
 (setq gc-cons-threshold 100000000)
-
+(setq read-process-output-max (* 1024 1024))
 (setq inhibit-startup-message t)
 
 (scroll-bar-mode -1)
@@ -42,7 +42,7 @@
 (use-package ido-completing-read+
   :config
   (ido-ubiquitous-mode 1))
-;;(setq ido-enable-flex-matching t)
+(setq ido-enable-flex-matching t)
 
 (use-package no-littering)
 
@@ -69,27 +69,37 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "2dd4951e967990396142ec54d376cced3f135810b2b69920e77103e0bcedfba9" default))
+   '("dc8285f7f4d86c0aebf1ea4b448842a6868553eded6f71d1de52f3dcbc960039" "ff24d14f5f7d355f47d53fd016565ed128bf3af30eb7ce8cae307ee4fe7f3fd0" "944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "2dd4951e967990396142ec54d376cced3f135810b2b69920e77103e0bcedfba9" default))
  '(helm-minibuffer-history-key "M-p")
  '(package-selected-packages
-   '(treemacs-evil treemacs-projectile tree-sitter yasnippet magit evil-magit rg tree-sitter-langs rustic company corfu eglot telephone-line dashboard no-littering evil-collection evil ivy helm-projectile projectile doom-themes which-key use-package)))
+   '(json-mode vterm tide rjsx-mode prettier-js fancy-compilation fancy-compilation-mode undo-tree hl-indent-scope diff-hl doom-modeline smart-mode-line solaire mood-line haskell-mode tuareg treemacs-evil treemacs-projectile tree-sitter yasnippet magit evil-magit rg tree-sitter-langs rustic company corfu eglot telephone-line dashboard no-littering evil-collection evil ivy helm-projectile projectile doom-themes which-key use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(hl-indent-scope-even-face ((t (:background "#51535f"))))
+ '(hl-indent-scope-odd-face ((t (:background "#3c3e4a")))))
 
 (use-package which-key
 	     :ensure t
 	     :config (which-key-mode))
 
 ;; Themes
-(use-package doom-themes
-	     :ensure t
-	     :config
-	     (load-theme 'doom-dracula))
+;;(use-package doom-themes
+;;	     :ensure t
+;;	     :config
+;;	     (load-theme 'doom-horizon))
 
+(use-package modus-themes
+  :ensure t
+  :init
+  (setq modus-themes-syntax '(green-strings yellow-comments)
+	modus-themes-mode-line '(accented borderless)
+	modus-themes-hl-line '(accented))
+  (modus-themes-load-themes)
+  :config
+(load-theme 'modus-vivendi t))
 
 ;; Projectile
 (use-package projectile
@@ -137,11 +147,18 @@
 
 
 ;; Telephone line
-(use-package telephone-line
-  :ensure t
-  :config
-  (telephone-line-mode 1))
+;;(use-package telephone-line
+  ;;:ensure t
+  ;;:config
+  ;;(telephone-line-mode 1))
 
+;;
+;;(use-package doom-modeline
+  ;;:ensure t
+  ;;:init (doom-modeline-mode 1)
+  ;;:config
+  ;;(setq doom-modeline-lsp t)
+  ;;(setq doom-modeline-time t))
 
 ;; lsp tings
 (use-package eglot
@@ -197,3 +214,55 @@
   :after (treemacs projectile)
   :ensure t)
 
+(use-package tuareg)
+
+(use-package dired
+  :ensure nil
+  :commands (dired dired-jump)
+  :bind (("C-x C-j" . dired-jump))
+  :config
+  (evil-collection-define-key 'normal 'dired-mode-map
+    "h" 'dired-up-directory
+    "l" 'dired-find-file))
+
+(use-package haskell-mode)
+
+(use-package diff-hl
+  :config
+  (global-diff-hl-mode))
+(use-package hl-indent-scope)
+
+;;(use-package undo-tree)
+
+(use-package fancy-compilation
+  :commands (fancy-compilation-mode))
+(with-eval-after-load 'compile
+  (fancy-compilation-mode))
+
+(use-package rjsx-mode
+  :ensure t
+  :mode "\\.js\\'")
+
+(defun setup-tide-mode()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save-mode-enabled))
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
+
+(use-package tide
+  :ensure t
+  :after (rjsx-mode company flycheck)
+  :hook (rjsx-mode . setup-tide-mode))
+
+(use-package prettier-js
+  :ensure t
+  :after (rjsx-mode)
+  :hook (rjsx-mode . prettier-js-mode))
+
+(use-package vterm
+  :ensure t)
+
+(use-package json-mode
+  :ensure t)
